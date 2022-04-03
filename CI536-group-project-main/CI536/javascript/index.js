8d7e892404e30a56bab46dde5763236f2b3d5955
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2022-04-03 13:01:24 +0100

    Started adapting login/ sign up to ideal design

--- CI536-group-project-main/CI536/javascript/index.js
+++ CI536-group-project-main/CI536/javascript/index.js
@@ -4,14 +4,14 @@ let current = 1;
 function tab2() {
     document.querySelector("#form").style.marginLeft="-100%";
     document.querySelector(".login").style.background = "none";
-    document.querySelector(".signup").style.background = "linear-gradient(45deg, #00d5fc, #046af6)";
+    document.querySelector(".signup").style.background = "linear-gradient(45deg, #8765fa, #e400ab)";
     document.querySelectorAll(".switch")[current - 1].classList.add("active");
 }
     
 function tab1() {
     document.querySelector("#form").style.marginLeft="0";
     document.querySelector(".signup").style.background = "none";
-    document.querySelector(".login").style.background = "linear-gradient(45deg, #00d5fc, #046af6)";
+    document.querySelector(".login").style.background = "linear-gradient(45deg, #8765fa, #e400ab)";
     document.querySelectorAll(".switch")[current - 1].classList.remove("active");
 }
 
