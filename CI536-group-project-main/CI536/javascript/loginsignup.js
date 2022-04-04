81d4b4bf3a77403c75df7721ed4459fd6001f274
jake-ward2639 <jake.ward2639@gmail.com>
2022-04-04 19:03:49 +0100

    more ids added

--- CI536-group-project-main/CI536/javascript/loginsignup.js
+++ CI536-group-project-main/CI536/javascript/loginsignup.js
@@ -1,11 +1,6 @@
 window.addEventListener('load', function(evt){
 
     var signupBtn = document.querySelector("#signup-btn")
-    loginUsername = document.querySelector("#login-username")
-    loginPassword = document.querySelector("#login-password")
-    signupUsername = document.querySelector("#signup-username")
-    signupPassword = document.querySelector("#signup-password")
-    signupEmail = document.querySelector("#email")
     loginBtn = document.querySelector("#login-btn");
 
     signupBtn.addEventListener('submit', function(evt){
@@ -15,6 +10,27 @@ window.addEventListener('load', function(evt){
 
     loginBtn.addEventListener('submit', function(evt){
         evt.preventDefault()
+        
+        var mysql = require('mysql');
+
+        var con = mysql.createConnection({
+        host: "localhost",
+        user: "yourusername",
+        password: "yourpassword",
+        database: "mydb"
+        });
+
+        var usernameT = document.querySelector("#login-username").value.trim();
+        var passwordT = document.querySelector("#login-password").value.trim();
+
+        con.connect(function(err) {
+            if (err) throw err;
+            con.query("SELECT * FROM Users WHERE Username = '" + usernameT + "' AND Password = '" + passwordT + "'", function (err, result) {
+              if (err) throw err;
+              console.log(result);
+            });
+        });
+
         //check fields are valid and submit
     })
 })
