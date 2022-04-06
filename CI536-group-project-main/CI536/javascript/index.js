1c34d1a96e37595733d5487834ade0245e0aec18
Tom Watson <82597157+SigherWolf@users.noreply.github.com>
2022-04-06 18:21:07 +0100

    Fixed icon positioning-glitch for smaller screens & added site logo in the top-middle of form container

--- CI536-group-project-main/CI536/javascript/index.js
+++ CI536-group-project-main/CI536/javascript/index.js
@@ -1,25 +1,41 @@
 let current = 1;
     
-function tab2() {
-    document.querySelector("#form").style.marginLeft="-100%";
-    document.querySelector(".login").style.background = "none";
-    document.querySelector(".signup").style.background = "linear-gradient(45deg, #8765fa, #e400ab)";
-    document.querySelectorAll(".switch")[current - 1].classList.add("active");
-}
-    
-function tab1() {
+/**
+ * Scrolls to the LOGIN form in 'loginsignup.html', which simultaneously hides the SIGN UP form.
+ */
+function scrollToLogin() {
     document.querySelector("#form").style.marginLeft="0";
     document.querySelector(".signup").style.background = "none";
     document.querySelector(".login").style.background = "linear-gradient(45deg, #8765fa, #e400ab)";
     document.querySelectorAll(".switch")[current - 1].classList.remove("active");
 }
 
-function togglePassword1(e) {
+/**
+ * Scrolls to the SIGN UP form in 'loginsignup.html', which simultaneously hides the LOGIN form.
+ */
+function scrollToSignUp() {
+    document.querySelector("#form").style.marginLeft="-100%";
+    document.querySelector(".login").style.background = "none";
+    document.querySelector(".signup").style.background = "linear-gradient(45deg, #8765fa, #e400ab)";
+    document.querySelectorAll(".switch")[current - 1].classList.add("active");
+}
+
+/**
+ * Toggles the password input type between 'password' and 'text' in the LOGIN form, allowing the
+ * user to see the characters they entered.  
+ * @param {Event} e - The event, which in this case is a click event 
+ */
+function toggleLoginPwd(e) {
     document.querySelector("#login-password").type = (document.querySelector("#login-password").type == "password") ? "text" : "password";
     e.classList.value = (e.classList.value == "far fa-eye") ? "far fa-eye-slash" : "far fa-eye"; 
 }
 
-function togglePassword2(e) {
+/**
+ * Toggles the password input type between 'password' and 'text' in the SIGN UP form, allowing the
+ * user to see the characters they entered.
+ * @param {Event} e - The event, which in this case is a click event 
+ */
+function toggleSignUpPwd(e) {
     document.querySelector("#signup-password").type = (document.querySelector("#signup-password").type == "password") ? "text" : "password";
     e.classList.value = (e.classList.value == "far fa-eye") ? "far fa-eye-slash" : "far fa-eye"; 
 }
