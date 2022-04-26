26f528e410cca53bd03abfeb33ca02d3af74b1b2
jake-ward2639 <80108795+jake-ward2639@users.noreply.github.com>
2022-04-26 17:00:42 +0100

    replacing with actual sql

--- CI536-group-project-main/Laptopia_Database/Laptopia_Database/Products.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Products]
-(
-	[ProductId] INT NOT NULL PRIMARY KEY, 
-    [Stock] INT NOT NULL, 
-    [ProductName] varchar(30) NOT NULL, 
-    [Description] varchar(100) NOT NULL, 
-    [ImageSrc] varchar(200) NOT NULL, 
-    [ProductPrice] DECIMAL(15, 2) NOT NULL
-)
--- CI536-group-project-main/Laptopia_Database/Laptopia_Database/UserPaymentMethods.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[UserPaymentMethods]
-(
-	[PaymentMethodId] INT NOT NULL PRIMARY KEY,
-	[UserId] INT NOT NULL,
-	[CardNum] INT NOT NULL, 
-    [ExpDate] DATE NOT NULL, 
-    [CVV] INT NOT NULL, 
-    FOREIGN KEY (UserId) REFERENCES Users(UserId)
-)
