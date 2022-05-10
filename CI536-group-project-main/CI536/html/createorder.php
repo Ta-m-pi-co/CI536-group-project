410d651689522ec8d4c4aadf9f84f3ec034f8ed1
jake-ward2639 <jake.ward2639@gmail.com>
2022-05-10 17:28:09 +0100

    brief email

--- CI536-group-project-main/CI536/html/createorder.php
+++ CI536-group-project-main/CI536/html/createorder.php
@@ -5,6 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
         $username = $_COOKIE[username];
         $userId;
+        $email;
         $fullname = urldecode($_POST[name]);
         $price = urldecode($_POST[price]);
         $basket = json_decode($_COOKIE[basket],true);
@@ -12,10 +13,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $conn = mysqli_connect('localhost', 'cw1019_admin', '0B+F4pp_~u{p', 'cw1019_laptopia_database');
         if($conn->connect_error == null){
             
-            $sql = "SELECT `UserId` FROM `Users` WHERE `Username` = '$username'";
+            $sql = "SELECT `UserId`, `Email` FROM `Users` WHERE `Username` = '$username'";
             $result = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($connection));
             $row = mysqli_fetch_assoc($result);
             $userId = $row[UserId];
+            $email = $row[Email];
             
             $sql = "INSERT INTO `Orders`(`UserId`, `FullName`, `DateOfOrder`, `OrderPrice`) VALUES ('$userId','$fullname','" . date("Y/m/d") . "','$price')";
             $success = mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($connection));
@@ -29,6 +31,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     mysqli_query($conn, $sql) or die("Error in Selecting " . mysqli_error($connection));
                 }
                 http_response_code(201);
+
+                $msg = "$orderId is your order number";
+                mail($email,"Laptopia: Reciept of Purchase",$msg);
             }
         }else{
             http_response_code(500);
